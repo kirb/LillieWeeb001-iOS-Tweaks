ec33a6c72d9375b486274fb6557ec0e0eab3aaec
Lillie <91358136+LillieWeeb001@users.noreply.github.com>
2021-12-23 17:13:52 -0500

    Why did I upload these

--- Enhancer for YouTube Premium/Tweak.h
+++
@@ -1,2 +0,0 @@
-@interface EnhancerforYouTubePremiumOptionsController : UIViewController <UITableViewDelegate, UITableViewDataSource>
-@end
--- Spotify Reborn/RootOptionsController.h
+++
@@ -1,7 +0,0 @@
-#import <UIKit/UIKit.h>
-
-extern UITextField *hexTextField;
-
-@interface RootOptionsController : UITableViewController
-
-@end
